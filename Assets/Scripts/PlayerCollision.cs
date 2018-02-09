54e6fb4c64e0d4420f09f6b5e1394d238b0c3be4
ishtian revee <ishtian11@gmail.com>
2018-02-09 22:40:55 +0600

    collision detection added

---
+++ Assets/Scripts/PlayerCollision.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+
+public class PlayerCollision : MonoBehaviour {
+
+    public Transform explosion;
+
+    // This method is called when the player collide with an object
+    void OnTriggerEnter2D(Collider2D colliderInfo)  // Get the information about the collision and call it 'collisionInfo'
+    {
+        // Checking if the player collide with has a tag called 'Obstackle'
+        if (colliderInfo.tag == "Planet")
+        {
+            //// making the player movement to false
+            //GetComponent<PlayerMovement>().enabled = false;
+
+            //// Calls the game over method from the gamemanager
+            //FindObjectOfType<GameManager>().GameOver();
+            Instantiate(explosion, transform.position, transform.rotation);
+            Destroy(this.gameObject);
+        }
+    }
+}
